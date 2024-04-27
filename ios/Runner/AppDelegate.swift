1e89eef8f5870547b1d1cbf1b3ad5e8e27f3e36d
Libmuy <libmuy@gmail.com>
2024-04-27 20:13:53 +0900

    platforms errors

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,17 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    var channelHandler: PlatformChannelsHandler?
+
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    channelHandler = PlatformChannelsHandler()
+      channelHandler?.initialize(window: window)
+	
+      GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
